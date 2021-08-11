39852329572a8914f99e689a821f9d0f6a8318bc
olv_jotta <klbvitor.a@gmail.com>
2021-08-11 20:36:10 -0300

    Subindo projeto inicial

---
+++ src/main/java/com/algaworks/entrega/api/MergulhoSpringAlgaworksEntregaApiApplication.java
@@ -0,0 +1,13 @@
+package com.algaworks.entrega.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MergulhoSpringAlgaworksEntregaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MergulhoSpringAlgaworksEntregaApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/algaworks/entrega/api/MergulhoSpringAlgaworksEntregaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.algaworks.entrega.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MergulhoSpringAlgaworksEntregaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
